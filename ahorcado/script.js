3c7406ab289c45968c51969d67943dd90a2e9471
filiprendon <126860088+filiprendon@users.noreply.github.com>
2023-12-19 23:19:52 +0100

    Lógica del juego

--- ahorcado/script.js
+++ ahorcado/script.js
@@ -1,42 +1,62 @@
 // Arrays y variables
+const imgAhorcado = document.querySelector(".ahorcado img");
+const displayPalabra = document.querySelector(".palabra");
+const textoErrores = document.querySelectorAll(".errores b")[0];
+const teclado = document.querySelector(".teclado");
 
-const teclado = document.querySelector('.teclado');
 
-const letrasAbecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 const palabras = [
-    'avión', 'playa', 'maleta', 'pasaporte', 'hotel', 'mapa', 'excursión', 'explorar', 'cultura', 'aventura',
-    'viajero', 'montaña', 'equipaje', 'guía', 'reserva', 'turismo', 'destino', 'travesía', 'adventure', 'descubrir',
-    'itinerario', 'naturaleza', 'tradiciones', 'monumento', 'océano', 'isla', 'exploración', 'fotografía', 'transporte'
+    'avioneta', 'playa', 'maleta', 'pasaporte', 'hotel', 'mapa', 'excursion', 'explorar', 'cultura', 'aventura',
+    'viajero', 'monte', 'equipaje', 'guia', 'reserva', 'turismo', 'destino', 'travesia', 'adventure', 'descubrir',
+    'itinerario', 'naturaleza', 'tradiciones', 'monumento', 'oceano', 'isla', 'exploración', 'fotografia', 'transporte'
 ];
 
-let palabraSeleccionada = '';
+let palabraActual, errores = 0;
+const maxErrores = 6;
 
-let letrasUsuario = [];
 
-const maxIntentos = 6;
 
-let errores = 0;
+const initGame = (button, letraClicada) => {
+    if (palabraActual.includes(letraClicada)) {
+        [...palabraActual].forEach((letra, index) => {
+            if (letra === letraClicada) {
+                displayPalabra.querySelectorAll("li")[index].innerText = letra;
+                displayPalabra.querySelectorAll("li")[index].classList.add("adivinada");
+            }
 
-letrasAbecedario.forEach(letra => {
-    const boton = document.createElement('button');
-    boton.textContent = letra;
-    boton.addEventListener('click', function() {
-        
-        console.log('Letra clicada:', letra);
-    });
-    teclado.appendChild(boton);
-});
+        });
+    }
+    else {
+        errores++;
+        imgAhorcado.src = `img/${errores}.png`;
+    }
+    button.disabled = true;
+    textoErrores.innerText = `${errores} / ${maxErrores}`;
+}
+
+// Creo un botón por cada letra del abecedario y lo imprimo por consola
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    teclado.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+
+}
+
+seleccionarPalabraRandom();
 
 
 // Funciones
+function seleccionarPalabraRandom() {
+    const palabraSeleccionada = palabras[Math.floor(Math.random() * palabras.length)];
 
-function seleccionarPalabraRandom(){
-   const indice = Math.floor(Math.random() * palabras.length);
-   palabraSeleccionada = palabras[indice].toLowerCase;
+    palabraActual = palabraSeleccionada;
+    displayPalabra.innerHTML = palabraSeleccionada.split("").map(() => `<li class="letra"></li>`).join("");
+    console.log(palabraSeleccionada);
 }
 
-function iniciarJuego(){
+function iniciarJuego() {
     window.location.href = "juegoAhorcado.html";
     seleccionarPalabraRandom();
     letrasUsuario = [];
@@ -44,27 +64,7 @@ function iniciarJuego(){
 
 }
 
-function ocultarPalabraSeleccionada(){
-    let palabraOculta = '';
-    for (const letra of palabraSeleccionada) {
-        if (letrasUsuario.includes(letra)) {
-            palabraOculta += letra + ' ';
-        } else {
-            palabraOculta += '_ ';
-        }
-    }
-    return palabraOculta.trim();
-}
 
-function verificarPalabraCompletada(){
-    for (const letra of palabraSeleccionada) {
-        if (!letrasUsuario.includes(letra)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function juegoPerdido(){
+function juegoPerdido() {
     return errores >= maxIntentos;
 }
