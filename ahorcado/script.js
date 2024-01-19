759d49002b117647295e1b6cbc83dd40c7faee96
filiprendon <frendont2223@politecnics.barcelona>
2024-01-19 18:35:55 +0100

    Actu

--- ahorcado/script.js
+++ ahorcado/script.js
@@ -1,5 +1,5 @@
 // Arrays y variables
-let palabraActual, infoPalabra, errores = 0;
+let palabraActual, infoPalabra, iamgenPalabra, errores = 0;
 const maxErrores = 6;
 const imgAhorcado = document.querySelector(".ahorcado img");
 const displayPalabra = document.querySelector(".palabra");
@@ -7,35 +7,35 @@ const textoErrores = document.querySelector(".errores b");
 const teclado = document.querySelector(".teclado");
 const verificacion = document.getElementsByClassName("letra adivinada");
 const palabrasCategorias = [
-    { palabra: "avioneta", categoria: "Transporte y Destinos", info: "Una pequeña aeronave utilizada para vuelos cortos." },
-    { palabra: "playa", categoria: "Transporte y Destinos", info: "Una extensión de arena y agua en la costa." },
-    { palabra: "monte", categoria: "Transporte y Destinos", info: "Una elevación de terreno más alta que una colina." },
-    { palabra: "destino", categoria: "Transporte y Destinos", info: "El lugar al que alguien se dirige o viaja." },
-    { palabra: "oceano", categoria: "Transporte y Destinos", info: "Una vasta extensión de agua salada." },
-    { palabra: "isla", categoria: "Transporte y Destinos", info: "Un pedazo de tierra rodeado de agua." },
-    { palabra: "crucero", categoria: "Transporte y Destinos", info: "Un viaje en barco de placer." },
-    { palabra: "excursion", categoria: "Actividades y Experiencias", info: "Un viaje breve con propósitos recreativos." },
-    { palabra: "explorar", categoria: "Actividades y Experiencias", info: "Descubrir un lugar desconocido." },
-    { palabra: "cultura", categoria: "Actividades y Experiencias", info: "Las creencias, valores y tradiciones de un grupo de personas." },
-    { palabra: "aventura", categoria: "Actividades y Experiencias", info: "Una experiencia emocionante y riesgosa." },
-    { palabra: "viajero", categoria: "Actividades y Experiencias", info: "Una persona que viaja por placer o negocio." },
-    { palabra: "turismo", categoria: "Actividades y Experiencias", info: "La industria de viajes y recreación." },
-    { palabra: "travesia", categoria: "Actividades y Experiencias", info: "Un viaje largo y difícil." },
-    { palabra: "descubrir", categoria: "Actividades y Experiencias", info: "Encontrar algo nuevo o desconocido." },
-    { palabra: "naturaleza", categoria: "Actividades y Experiencias", info: "El mundo natural, incluyendo plantas y animales." },
-    { palabra: "tradiciones", categoria: "Actividades y Experiencias", info: "Prácticas culturales transmitidas de generación en generación." },
-    { palabra: "monumento", categoria: "Actividades y Experiencias", info: "Una estructura con valor histórico o cultural." },
-    { palabra: "exploracion", categoria: "Actividades y Experiencias", info: "La acción de investigar o descubrir territorios desconocidos." },
-    { palabra: "fotografia", categoria: "Actividades y Experiencias", info: "El arte de capturar imágenes con una cámara." },
-    { palabra: "maleta", categoria: "Equipamiento y Preparativos", info: "Un contenedor para llevar ropa y pertenencias en un viaje." },
-    { palabra: "pasaporte", categoria: "Equipamiento y Preparativos", info: "Un documento de identificación para viajar internacionalmente." },
-    { palabra: "hotel", categoria: "Equipamiento y Preparativos", info: "Un lugar para alojarse durante un viaje." },
-    { palabra: "mapa", categoria: "Equipamiento y Preparativos", info: "Una representación gráfica de una región geográfica." },
-    { palabra: "equipaje", categoria: "Equipamiento y Preparativos", info: "Las pertenencias que llevas contigo en un viaje." },
-    { palabra: "guia", categoria: "Equipamiento y Preparativos", info: "Un libro o persona que proporciona información sobre un lugar." },
-    { palabra: "reserva", categoria: "Equipamiento y Preparativos", info: "Hacer una reserva para asegurar un servicio o alojamiento." },
-    { palabra: "itinerario", categoria: "Equipamiento y Preparativos", info: "Un plan detallado de actividades en un viaje." },
-    { palabra: "transporte", categoria: "Equipamiento y Preparativos", info: "Los medios utilizados para moverse de un lugar a otro." }
+    { palabra: "avioneta", categoria: "Transporte y Destinos", info: "Una pequeña aeronave utilizada para vuelos cortos.", img: "img/avioneta.jpg"},
+    { palabra: "playa", categoria: "Transporte y Destinos", info: "Una extensión de arena y agua en la costa.", img: "img/playa.jpg" },
+    { palabra: "monte", categoria: "Transporte y Destinos", info: "Una elevación de terreno más alta que una colina.", img: "img/monte.jpeg" },
+    { palabra: "destino", categoria: "Transporte y Destinos", info: "El lugar al que alguien se dirige o viaja.", img: "img/" },
+    { palabra: "oceano", categoria: "Transporte y Destinos", info: "Una vasta extensión de agua salada.", img: "img/oceano.jpg" },
+    { palabra: "isla", categoria: "Transporte y Destinos", info: "Un pedazo de tierra rodeado de agua.", img: "img/isla.jpg" },
+    { palabra: "crucero", categoria: "Transporte y Destinos", info: "Un viaje en barco de placer.", img: "img/crucero.png" },
+    { palabra: "excursion", categoria: "Actividades y Experiencias", info: "Un viaje breve con propósitos recreativos.", img: "img/excursion.jpg" },
+    { palabra: "explorar", categoria: "Actividades y Experiencias", info: "Descubrir un lugar desconocido.", img: "img/explorar" },
+    { palabra: "cultura", categoria: "Actividades y Experiencias", info: "Las creencias, valores y tradiciones de un grupo de personas.", img: "img/cultura" },
+    { palabra: "aventura", categoria: "Actividades y Experiencias", info: "Una experiencia emocionante y riesgosa.", img: "img/aventura" },
+    { palabra: "viajero", categoria: "Actividades y Experiencias", info: "Una persona que viaja por placer o negocio.", img: "img/viajero" },
+    { palabra: "turismo", categoria: "Actividades y Experiencias", info: "La industria de viajes y recreación.", img: "img/turismo" },
+    { palabra: "travesia", categoria: "Actividades y Experiencias", info: "Un viaje largo y difícil.", img: "img/travesia" },
+    { palabra: "descubrir", categoria: "Actividades y Experiencias", info: "Encontrar algo nuevo o desconocido.", img: "img/descubrir" },
+    { palabra: "naturaleza", categoria: "Actividades y Experiencias", info: "El mundo natural, incluyendo plantas y animales.", img: "img/naturaleza" },
+    { palabra: "tradiciones", categoria: "Actividades y Experiencias", info: "Prácticas culturales transmitidas de generación en generación.", img: "img/tradiciones" },
+    { palabra: "monumento", categoria: "Actividades y Experiencias", info: "Una estructura con valor histórico o cultural.", img: "img/monumento.jpg" },
+    { palabra: "exploracion", categoria: "Actividades y Experiencias", info: "La acción de investigar o descubrir territorios desconocidos.", img: "img/exploracion.jpg" },
+    { palabra: "fotografia", categoria: "Actividades y Experiencias", info: "El arte de capturar imágenes con una cámara.", img: "img/fotografia.jpg" },
+    { palabra: "maleta", categoria: "Equipamiento y Preparativos", info: "Un contenedor para llevar ropa y pertenencias en un viaje.", img: "img/maleta.jpg" },
+    { palabra: "pasaporte", categoria: "Equipamiento y Preparativos", info: "Un documento de identificación para viajar internacionalmente.", img: "img/pasaporte.jpg" },
+    { palabra: "hotel", categoria: "Equipamiento y Preparativos", info: "Un lugar para alojarse durante un viaje.", img: "img/hotel.jpg" },
+    { palabra: "mapa", categoria: "Equipamiento y Preparativos", info: "Una representación gráfica de una región geográfica.", img: "img/mapa.jpg" },
+    { palabra: "equipaje", categoria: "Equipamiento y Preparativos", info: "Las pertenencias que llevas contigo en un viaje.", img: "img/equipaje.jpg" },
+    { palabra: "guia", categoria: "Equipamiento y Preparativos", info: "Un libro o persona que proporciona información sobre un lugar.", img: "img/guia.jpeg" },
+    { palabra: "reserva", categoria: "Equipamiento y Preparativos", info: "Hacer una reserva para asegurar un servicio o alojamiento.", img: "img/reserva.jpg" },
+    { palabra: "itinerario", categoria: "Equipamiento y Preparativos", info: "Un plan detallado de actividades en un viaje.", img: "img/itinerario" },
+    { palabra: "transporte", categoria: "Equipamiento y Preparativos", info: "Los medios utilizados para moverse de un lugar a otro.", img: "img/transporte.jpg" }
 ];
 
 
@@ -82,15 +82,17 @@ guardarProgreso();
 
 // Funciones
 function seleccionarPalabraRandom() {
-    const { palabra, categoria, info } = palabrasCategorias[Math.floor(Math.random() * palabrasCategorias.length)];
+    const { palabra, categoria, info, img } = palabrasCategorias[Math.floor(Math.random() * palabrasCategorias.length)];
     palabraActual = palabra;
     infoPalabra = info;
+    iamgenPalabra = img;
     document.querySelector(".categoria b").innerHTML = categoria;
     displayPalabra.innerHTML = palabra.split("").map(() => `<li class="letra"></li>`).join("");
 
     console.log(palabraActual);
     console.log(categoria);
-    console.log(info);
+    console.log(infoPalabra);
+    console.log(iamgenPalabra);
     guardarProgreso();
 }
 
@@ -98,7 +100,8 @@ function iniciarJuego() {
     window.location.href = "juegoAhorcado.html";
     seleccionarPalabraRandom();
     letrasUsuario = [];
-    errores = 0
+    errores = 0;
+    cargarProgreso();
 }
 
 function verificarErrores() {
@@ -113,17 +116,20 @@ function verificarSolucion() {
     }
 }
 
-// Pq no va info
 function popupjuegoGanado() {
     document.getElementById('juegoGanado').style.display = "block";
-    document.getElementById('txtGanado').innerHTML = "Felicidades, has acertado la palabra";
-    document.getElementById('info').innerHTML = infoPalabra;
+    document.querySelector('.card-title').innerHTML = palabraActual;
+    document.querySelector('.card-text').innerHTML = infoPalabra;
+    document.querySelector('.img-fluid').src = iamgenPalabra;
+    // document.getElementById('txtGanado').innerHTML = "Felicidades, has acertado la palabra";
+    
 }
 
 function popupjuegoPerdido() {
     document.getElementById('juegoPerdido').style.display = "block";
-    document.getElementById('txtPerdido').innerHTML = "Has perdido, la palabra era: " + palabraActual;
-    document.getElementById('info2').innerHTML = infoPalabra;
+    document.querySelector('.card-title').innerHTML = palabraActual;
+    document.querySelector('.card-text').innerHTML = infoPalabra;
+    document.querySelector('.img-fluid').src = iamgenPalabra;
 }
 
 function cerrarPopup() {
@@ -139,76 +145,43 @@ function volverAJugar() {
 
 
 // localStorage
-function guardarProgreso(){
-    const estadoDelJuego = {
-        nombreUsuario: document.getElementById("nombreUsuario"),
+function guardarProgreso() {
+    const estadoJuego = {
         palabraActual: palabraActual,
-        // letrasAdivinadas: letraClicada,
-        erroresActuales: errores
+        errores: errores,
+        letrasAdivinadas: Array.from(document.querySelectorAll(".adivinada")).map(el => el.textContent),
+    };
+    localStorage.setItem('progresoJuego', JSON.stringify(estadoJuego));
+}
+
+function cargarProgreso() {
+    const datosGuardados = localStorage.getItem('progresoJuego');
+    if (datosGuardados) {
+        const estadoJuego = JSON.parse(datosGuardados);
+        palabraActual = estadoJuego.palabraActual;
+        errores = estadoJuego.errores;
+        reconstruirEstadoJuego(estadoJuego);
+    } else {
+        iniciarJuego();
     }
-    localStorage.setItem('progresoJuego', JSON.stringify(estadoDelJuego));
 }
 
 
+function reconstruirEstadoJuego(estadoJuego) {
+    displayPalabra.innerHTML = palabraActual.split("").map(letra => 
+        `<li class="letra ${estadoJuego.letrasAdivinadas.includes(letra) ? 'adivinada' : ''}">${estadoJuego.letrasAdivinadas.includes(letra) ? letra : ''}</li>`).join("");
 
+    imgAhorcado.src = `img/${errores}.png`;
+    textoErrores.innerText = `${errores} / ${maxErrores}`;
+    const botonesTeclado = teclado.querySelectorAll("button");
+    botonesTeclado.forEach(button => {
+        if (estadoJuego.letrasAdivinadas.includes(button.textContent) || palabraActual.includes(button.textContent) === false) {
+            button.disabled = true;
+            button.style.backgroundColor = palabraActual.includes(button.textContent) ? "green" : "red";
+        }
+    });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function guardarProgreso() {
-//     const estadoJuego = {
-//         palabraActual: palabraActual,
-//         errores: errores,
-//         letrasAdivinadas: Array.from(document.querySelectorAll(".adivinada")).map(el => el.textContent),
-//     };
-//     localStorage.setItem('progresoJuego', JSON.stringify(estadoJuego));
-// }
-// function cargarProgreso() {
-//     const datosGuardados = localStorage.getItem('progresoJuego');
-//     if (datosGuardados) {
-//         const estadoJuego = JSON.parse(datosGuardados);
-//         palabraActual = estadoJuego.palabraActual;
-//         errores = estadoJuego.errores;
-//         reconstruirEstadoJuego(estadoJuego);
-//     } else {
-//         seleccionarPalabraRandom();
-//     }
-// }
-
-// function borrarProgreso(){
-//     localStorage.removeItem('progresoJuego');
-// }
+function borrarProgreso(){
+    localStorage.removeItem('progresoJuego');
+}
